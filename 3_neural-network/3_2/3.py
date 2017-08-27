19c11e4105ff18871b3d78bf2395ca2602371372
araki-ka <araki-ka@legendapl.com>
2017-08-27 23:28:27 +0900

    Revert "ステップ関数を関数化"
    This reverts commit c87af64a781cce8d29dd3c11d947b88d63c5dce9.

--- 3_neural-network/3_2/3.py
+++ 3_neural-network/3_2/3.py
@@ -4,11 +4,14 @@
 # 3.2.3 ステップ関数のグラフ
 import numpy as np
 import matplotlib.pylab as plt
-from functions import step_function as stp
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
 
 
 x = np.arange(-5.0, 5.0, 0.1)
-y = stp.step_function(x)
+y = step_function(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
 plt.show()
