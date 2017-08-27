9c97b03934a103cfed0a9c8e4b29b26656dd010d
araki-ka <araki-ka@legendapl.com>
2017-08-27 23:22:27 +0900

    ステップ関数を関数化

--- 3_neural-network/3_2/3.py
+++ 3_neural-network/3_2/3.py
@@ -4,14 +4,11 @@
 # 3.2.3 ステップ関数のグラフ
 import numpy as np
 import matplotlib.pylab as plt
-
-
-def step_function(x):
-    return np.array(x > 0, dtype=np.int)
+from functions import step_function as stp
 
 
 x = np.arange(-5.0, 5.0, 0.1)
-y = step_function(x)
+y = stp.step_function(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
 plt.show()
